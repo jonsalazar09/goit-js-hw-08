ce264577aa867b831e5cfffa32821679256e1453
jonsalazar09 <jonsalazar09@gmail.com>
2024-05-07 18:47:04 +0800

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,7 +2,7 @@ import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
 // Add imports above this line
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from './gallery-items';
 // Change code below this line
 const imageGallery = document.querySelector('.gallery');
 const cardsMarkup = createImageCardsMarkup(galleryItems);
